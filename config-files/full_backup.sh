4e32c8f70a43284a8c6bfdc7d56ddb5a8e58885e
spencer.trinh <spencer.trinh@kinnate.com>
2023-02-09 21:48:22 -0800

    create pfile instead, rman logs path exapnsion

--- config-files/full_backup.sh
+++ config-files/full_backup.sh
@@ -1,6 +1,5 @@
 #! /bin/sh
 export DATE=$(date +%Y_%m_%d)
-export TIMESTAMP=$(date +%Y-%m-%d_%H%M%S)
 export ORACLE_HOME=/u01/app/oracle/product/19.3/db_home
 export ORACLE_SID=orcl_dm
 export PATH=$PATH:$ORACLE_HOME/bin
@@ -9,22 +8,18 @@ S3B=DevOps/rman_backups
 AWS=/usr/local/bin/aws
 
 fullbackup() {
-	rman log='${BACKUP}/bkpscripts/b_${DATE}_full_bkp.log' <<EOF
-
+	rman log="${BACKUP}/bkpscripts/b_${DATE}_full_bkp.log" <<EOF
 connect target /
-
 set echo on;
-
 run
 {
-
 backup incremental level 0 
 cumulative device type disk 
-format '${BACKUP}/autobackup/full_dm_bkup.bkp'
+format '${BACKUP}/autobackup/full_dm_%d_%T.bkp'
 tag 'ORA_DM' database;
-backup device type disk tag 'ORA_DM' 
-format = '${BACKUP}/archivelogs/${DATE}/%d_%u' 
-archivelog all not backed up delete all input;
+backup device type disk tag 'ORA_DM'
+format '${BACKUP}/autobackup/full_dm_archivelog_%d_%T.bkp'
+archivelog not backed up delete all input;
 delete noprompt obsolete device type disk;
 }
 exit
@@ -32,14 +27,24 @@ EOF
 }
 
 pfile() {
-        rm -rf $BACKUP/autobackup/*
-	#mkdir -p $BACKUP/autobackup/$DATE
-	mkdir -p $BACKUP/archivelogs/$DATE
-	sqlplus / as sysdba <<EOF
-		create spfile='${BACKUP}/autobackup/spfileorcl_dm.ora' from pfile='${ORACLE_HOME}/dbs/spfileorcl_dm.ora';
-		exit
-EOF
+	rm -rf $BACKUP/autobackup/*
+	sqlplus / as sysdba <<EOF > "pfile_output"
+		create pfile='${BACKUP}/autobackup/pfileorcl_dm.ora' from memory;
+	EOF
+	if grep -q "ORA-" "pfile_output" || grep -q "SP2-" "pfile_output"; then
+				return 1
+	else return 0
+	fi
 }
+
+# pfile_try() {
+# 	sqlplus / as sysdba <<EOF
+#   create pfile='${BACKUP}/autobackup/pfileorcl_dm.ora' from memory;
+# 	shutdown immediate;
+# 	startup pfile='${ORACLE_HOME}/dbs/initorcl_dm.ora';
+# 	create spfile from pfile;
+# 	EOF
+# }
 
 uploadbackup() {
 	check=$($AWS s3api list-objects-v2 --bucket fount-data --prefix "${S3B}/${DATE}" --query 'Contents[]')
@@ -58,11 +63,16 @@ uploadbackup() {
 			--bucket fount-data \
 			--key "${file}" \
 			--tagging '{"TagSet": [{ "Key": "Name", "Value": "RMAN" }]}'
-	#echo "${AWS} s3api put-object-tagging --bucket fount-data --key ${file}"
+		#echo "${AWS} s3api put-object-tagging --bucket fount-data --key ${file}"
 	done
 }
 
 #MAIN
 pfile
+exit_code=$?
+echo $exit_code
+# if [ $exit_code -eq 1 ]; then
+# 	pfile_try
+# fi
 fullbackup
 uploadbackup
