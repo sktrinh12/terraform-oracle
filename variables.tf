6a000b189813d87d879c2144de1127ac2075959a
spencer.trinh <spencer.trinh@kinnate.com>
2023-03-16 14:02:45 -0700

    add dotmatics pem

--- variables.tf
+++ variables.tf
@@ -1,14 +1,14 @@
 variable "awsprops" {
     type = map
     default = {
-    count = 2
+    count = 1
     region = "us-west-2"
     ami = "ami-0bf3b3e2db4302789" // OL7.9-x86_64-HVM-2020-12-07
     vpc = "vpc-031ebba25c9df51eb"
     itype = "r4.large"
     subnet = "subnet-086ffcb03661d67a2"
     publicip = false
-    keyname = "eks-apps" 
+    keyname = "dotmaticsdb" 
     volume_size =  250
     volume_type = "gp3"
   }
