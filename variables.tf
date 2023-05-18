cb6125fbb8d3268e99a800de2a570291eb507548
spencer.trinh <spencer.trinh@kinnate.com>
2023-05-17 19:54:53 -0700

    remove root block volume from list

--- variables.tf
+++ variables.tf
@@ -1,15 +1,15 @@
 variable "awsprops" {
     type = map
     default = {
-    count = 2
+    count = 1
     region = "us-west-2"
     ami = "ami-0bf3b3e2db4302789" // OL7.9-x86_64-HVM-2020-12-07
     vpc = "vpc-031ebba25c9df51eb"
     itype = "r4.large"
-    subnet = "subnet-086ffcb03661d67a2"
+    subnet = "subnet-086ffcb03661d67a2" // us-west-2b
     publicip = false
     keyname = "dotmaticsdb" 
-    volume_size =  250
+    volume_size =  100
     volume_type = "gp3"
   }
 }
@@ -41,15 +41,15 @@ variable "sns_topic_arn" {
 
 variable "disk_names" {
   type = list(string)
-  default = ["/dev/xvda1", "/dev/xvdb", "/dev/xvdc"]
+  default = ["/dev/xvdb", "/dev/xvdc"]
 }
 
 variable "disk_sizes" {
   type = list(number)
-  default = [50, 120, 250]
+  default = [150, 300]
 }
 
 variable "disk_iops" {
   type = list(number)
-  default = [100, 200, 300]
+  default = [200, 150]
 }
