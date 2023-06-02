11b7a4e0c84eaa913b3ac50037f5883bdaa1223b
spencer.trinh <spencer.trinh@kinnate.com>
2023-06-02 11:55:47 -0700

    disk iops as static value

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "awsprops" {
     type = map
     default = {
-    count = 1
+    count = 2
     region = "us-west-2"
     ami = "ami-0bf3b3e2db4302789" // OL7.9-x86_64-HVM-2020-12-07
     vpc = "vpc-031ebba25c9df51eb"
@@ -50,6 +50,6 @@ variable "disk_sizes" {
 }
 
 variable "disk_iops" {
-  type = list(number)
-  default = [200, 150]
+  type = number
+  default = 3000
 }
